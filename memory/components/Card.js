4b4e445fb94d3c8f186070d94d3f08df15dbbaf1
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-10 09:38:25 -0600

    Setup react app and possible components also rough explination of what each component should do

---
+++ memory/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// This function will mostly exist as a compainer that we will nest characters inside and style accordingly
+
+function Card(props){
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ memory/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Simply a header
+
+function Header (props){
+
+    return(
+        <div>
+
+        </div>
+    )
+}
