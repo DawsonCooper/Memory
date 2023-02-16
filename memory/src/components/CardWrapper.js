63fd59393cd082b582ba99eb061d48b24aae0157
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-16 13:34:10 -0600

    Implemented api with flags and capitals of all countries using axious, added base cards and a generate button

--- memory/src/components/CardWrapper.js
+++ memory/src/components/CardWrapper.js
@@ -1,112 +1,174 @@
 import React, { useState, useEffect } from 'react';
 import Character from './Character';
 import '../styles/Card.css';
+import Axios from 'axios';
 
 export default function CardWrapper(props){
+    let cardsBaseCase = [{
+        capital: "",
+        name: "",
+        key: "1",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "2",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "3",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "4",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "5",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "6",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "7",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "8",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "9",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "10",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "11",
+        flag: "",
+        punc: ''
+    },
+    {
+        capital: "",
+        name: "",
+        key: "12",
+        flag: "",
+        punc: ''
+    }]
+
     const {score, best, itemClicked} = props
-    let cardArray =[
-        {
-            key: '1',
-            image: require('../media/japan.jpg'),
-            alt: 'Tokyo',
-            charHead: 'Tokyo, Japan'
-        },
-        {
-            key: '2',
-            image: require('../media/czech.jpg'),
-            alt: 'Prague',
-            charHead: 'Prague, Czech'
-        },
-        {
-            key: '3',
-            image: require('../media/france.jpg'),
-            alt: 'Paris',
-            charHead: 'Paris, France'
-        },
-        {
-            key: '4',
-            image: require('../media/italy.jpg'),
-            alt: 'Rome',
-            charHead: 'Rome, Italy'
-        },
-        {
-            key: '5',
-            image: require('../media/newzealand.jpg'),
-            alt: 'Wellington',
-            charHead: 'Wellington, New Zealand'
-        },
-        {
-            key: '6',
-            image: require('../media/norway.jpg'),
-            alt: 'Oslo',
-            charHead: 'Oslo, Norway'
-        },
-        {
-            key: '7',
-            image: require('../media/poland.jpg'),
-            alt: 'Warsaw',
-            charHead: 'Warsaw, Poland'
-        },
-        {
-            key: '8',
-            image: require('../media/spain.jpg'),
-            alt: 'Madrid',
-            charHead: 'Madrid, Spain'
-        },
-        {
-            key: '9',
-            image: require('../media/sweeden.jpg'),
-            alt: 'Stockholm',
-            charHead: 'Stockholm, Sweeden'
-        },
-        {
-            key: '10',
-            image: require('../media/switzerland.jpg'),
-            alt: 'Bern',
-            charHead: 'Bern, Switzerland'
-        },
-        {
-            key: '11',
-            image: require('../media/ukraine.jpg'),
-            alt: 'Kiev',
-            charHead: 'Kiev, Ukraine'
-        },
-        {
-            key: '12',
-            image: require('../media/UnitedKingdom.jpg'),
-            alt: 'London',
-            charHead: 'London, United Kingdom'
-        }
-    ];
-    const [cards, modCards] = useState([])
+    
+    const [cards, modCards] = useState(cardsBaseCase)
+    let cardArray = [];
+    
+    let tempArr = [];
+    let tempCountry = [] 
+    const getCountries = () => {
+        modCards(cardsBaseCase)
+        tempCountry = []
+        Axios.get("https://countriesnow.space/api/v0.1/countries/flag/images").then(
+            (response) => {
+                if (response.status === 200){
+                     
+                    // Get countries name and thier flags
+                    for(let i = 0; i < 12; i++){
+                        let random = Math.floor(Math.random() * response.data.data.length)
+                        if (!tempArr.includes(random)){
+                            tempArr.push(random);
+                        }
+
+                    }
+                    for(let i = 0; i < tempArr.length; i++){
+                        tempCountry.push(response.data.data[tempArr[i]]);
+                        tempCountry[i].key = i;
+                        Axios.post('https://countriesnow.space/api/v0.1/countries/capital', {
+                            "country": tempCountry[i].name
+                        }).then(response => {
+                            response.data.data.capital ? tempCountry[i].capital = response.data.data.capital :  tempCountry[i].capital = "No defined capital";
+                            tempCountry[i].punc = ","
+                        })
+                    }
+                    // GET EXISTING COUNTRYS CAPITALS AND ADD TO COUNTRY ARR
+
+
+                            
+                }
+                setTimeout(() => {
+                    cardArray = tempCountry
+                    modCards(tempCountry)
+                }, 500)
+                 
+            })           
+    }
+    
+    
     useEffect(() => {
         console.log('useEffect')
-        let arr=[...cardArray];
-        let currentIndex = arr.length,  randomIndex;
-
-        while (currentIndex !== 0) {
-      
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
         
-            // And swap it with the current element.
-            [arr[currentIndex], arr[randomIndex]] = [
-              arr[randomIndex], arr[currentIndex]];
-          }
-        console.log('newArr',arr)
-        modCards(arr)
-        console.log('cardArr', cardArray)
-        console.log('--------------------------------------')
+        if(cardArray !== undefined){
+            let arr=[...cards];
+            let currentIndex = arr.length,  randomIndex;
+
+            while (currentIndex !== 0) {
+            
+                // Pick a remaining element.
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+            
+                // And swap it with the current element.
+                [arr[currentIndex], arr[randomIndex]] = [
+                  arr[randomIndex], arr[currentIndex]];
+            }
+            modCards(arr)
+        }
+        console.log('------------------------------')
     },[score, best])
+    
     return (
+        <>
+        <button onClick={getCountries} className='gen-btn'>Generate countries</button>
         <section className='cards-wrapper'>
+            
             {cards.map((card) =>(
                 <div className='card' key={card.key} onClick={() => {itemClicked(card.key)}}>
-                        <Character  key={card.key} image={ card.image } alternative={card.alt} charHead={card.charHead} />
+                        <Character  key={card.key} image={ card.flag } alternative={`${card.capital}${card.punc} ${card.name}`} charHead={`${card.capital}${card.punc} ${card.name}`} />
                 </div>
                 )
             )}
         </section>
+        </>
     )
 }
