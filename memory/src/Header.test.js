82aa2b8f150182cd9155b21b9663c7250a438459
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-21 09:33:48 -0600

    Setting up for testing (still having issues) / Testing diffrent router types with hosting

---
+++ memory/src/Header.test.js
@@ -0,0 +1,25 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import Header from "./components/Header";
+
+afterEach(cleanup);
+//describe('App Component', () => {
+//  it('renders a mixed list of countries', () =>{
+//    const { container } = render(<App />);
+//    expect(container).toMatchSnapshot();
+//  });
+//  it('shuffles countries after button click', () =>{
+//    render(<App />);
+//    const button = screen.getByRole("button", {name: 'Generate countries'})
+//    userEvent.click(button);
+//    const cardImg = screen.getAllByRole('img')
+//  })
+//  
+//});
+
+it('displays title', () => {
+  const { getByTestId, getByText } = render(<Header />)
+  expect(getByTestId('home-title')).toHaveTextContent('Memory  Game')
+})
--- memory/src/RouteSwitch.js
+++ memory/src/RouteSwitch.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import MemorySortComponent from "./components/MemorySort/Sort";
 
 const RouteSwitch = () => {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/sort" element={<MemorySortComponent />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 };
 
