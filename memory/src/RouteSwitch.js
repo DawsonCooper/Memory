ef29505cf56da547b0eca5a9af26ff0864de00c0
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-22 09:45:56 -0600

    Still trying to fix gh pages

--- memory/src/RouteSwitch.js
+++ memory/src/RouteSwitch.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import MemorySortComponent from "./components/MemorySort/Sort";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-      <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/sort" element={<MemorySortComponent />} />
       </Routes>
-      </HashRouter>
     </BrowserRouter>
   );
 };
