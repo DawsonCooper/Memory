f590f6fa506c7f4bb1ac5744caa63d0d74fa50c3
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-22 07:51:32 -0600

    Reseting routers to work on ghpahes

--- memory/src/RouteSwitch.js
+++ memory/src/RouteSwitch.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import MemorySortComponent from "./components/MemorySort/Sort";
 
 const RouteSwitch = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/sort" element={<MemorySortComponent />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
