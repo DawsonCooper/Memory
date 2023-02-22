62061879ffd7b25148e53292234791b1e64979e1
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-22 09:34:38 -0600

    Fixing gh pages again bug with routers

--- memory/src/setupTests.js
+++ memory/src/setupTests.js
@@ -3,6 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import userEvent from "@testing-library/user-event";
-import React from "react";
-import {  } from "@testing-library/react";
+
