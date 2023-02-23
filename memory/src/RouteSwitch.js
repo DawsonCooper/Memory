73025428b55a0654e3c1124741ea8780c35912a2
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-23 08:08:31 -0600

    Changing router type to hash

--- memory/src/RouteSwitch.js
+++ memory/src/RouteSwitch.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import MemorySortComponent from "./components/MemorySort/Sort";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<App />} />
         <Route exact path="/sort" element={<MemorySortComponent />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
