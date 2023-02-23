f76ed293e64bd33e589627bf00536d7debbd87b4
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-02-23 16:03:58 -0600

    Added files for menu components that will act as our navigation

---
+++ memory/src/components/Menu/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MenuComponent from './Menu.component';
+// This componenet should be a clickable svg or boxicon that when clicked will open up a menucontent componenet
+export default function Menu(props){
+
+    return(
+        <>
+        </>
+    )
+}
---
+++ memory/src/components/Menu/MenuContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// This component should have the content we want for out navigation menu mainly out links back and forth between pages 
+// We will likely need some conditional rendering to see which page we are currenly on do disable or hide nav links to the page we are already on
+
+export default function MenuContent(props){
+
+    return (
+        <>
+        </>
+    )
+}
