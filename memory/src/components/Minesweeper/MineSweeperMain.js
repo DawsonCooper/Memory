52be475241cd18060ce9da305fa1f539173faa15
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-11-24 16:51:58 -0600

    Added reset button that resets all of the cells but does not reset the clicked values in the cells because of that the entire board we will likely want to setup some code that allows our cells to tell when the reset button is clicked and when it is clicked just set that cells clicked state back to false

--- memory/src/components/Minesweeper/MineSweeperMain.js
+++ memory/src/components/Minesweeper/MineSweeperMain.js
@@ -1,20 +1,137 @@
-import React/*, { useState, useContext }*/ from 'react';
+import React, {useEffect, useState}/*, { useState, useContext }*/ from 'react';
 import '../../styles/MinesweeperStyles/minesweeper.css';
 import Menu from '../Menu/Menu';
-import Gameboard from './MinesweeperBoard';
+import MinesweeperBoard from './MinesweeperBoard';
 import '../../styles/Menu/MenuContent.css';
-
+import MineCell from './MineCell.js';
+import SafeCell from './SafeCell.js';
+import '../../styles/MinesweeperStyles/gameboard.css';
 
 export default function MineSweeperMain(props){
+    const height = 8;
+    const width = 8;
+    let mines = []
+    let gameCells = []
+    const [gameBoard, setGameBoard] = useState([]); 
+    function generateBoard(){
+        // create an array of strings that represent the location of mines
 
+          for(let i = 0; i < 8; i++){
+            let ran1 = Math.floor(Math.random() * height);
+            let ran2 = Math.floor(Math.random() * width);
+            while(true){
+              if(mines.includes(`${ran1}${ran2}`)){
+                ran1 = Math.floor(Math.random() * height);
+                ran2 = Math.floor(Math.random() * width);
+              }else{
+                // creates out mines as a string to avoind issues with comparing arrays or sets in js
+                mines.push(`${ran1}${ran2}`);
+                break;
+              }
+            }
+          }
+      
+          // create board given mines and dimensions
+          for(let i = 0; i < height; i++){
+            gameCells[i] = []
+            for(let j =0; j < width; j++){
+      
+              if (mines.includes(`${i}${j}`)){
+                gameCells[i].push(
+                  <MineCell key={[i,j]} i={i} j={j} state={-1}/>
+                )
+              }
+              else{
+                gameCells[i].push(
+                <SafeCell key={[i,j]} i={i} j={j} count={0} state = {1}/>
+                
+                )
+              }
+            }
+          }
+      
+          // visit all neighbors of each mine and increase their value by 1
+          for(let i = 0; i < mines.length; i++){
+      
+            // get the index of the mine itself 
+            let index = [+mines[i][0],+mines[i][1]];
+      
+      
+            let mine = gameCells[index[0]][index[1]];
+      
+            // these will be the index of all real neighbors out of bounds will remain 0 
+            let left = undefined;
+            let right = undefined;
+            let up = undefined;
+            let down = undefined;
+            let upLeft =undefined;
+            let upRight = undefined;
+            let downLeft = undefined;
+            let downRight = undefined;
+      
+            if (index[1]-1 < 8 && index[1]-1 >= 0){
+              left = [index[0], index[1]-1];
+            }
+            if (index[1]+1 < 8 && index[1]+1 >= 0){
+              right = [index[0], index[1]+1];
+            }
+            if (index[0]-1 < 8 && index[0]-1 >= 0){
+              up = [index[0]-1,index[1]];
+            }
+            if (index[0]+1 < 8 && index[0]+1 >= 0){
+              down = [index[0]+1,index[1]];
+            }
+            if (index[0]-1 < 8 && index[0]-1 >= 0 && index[1]-1 < 8 && index[1]-1 >= 0){
+              upLeft = [index[0]-1,index[1]-1];
+            }
+            if (index[0]-1 < 8 && index[0]-1 >= 0 && index[1]+1 < 8 && index[1]+1 >= 0){
+              upRight = [index[0]-1,index[1]+1];
+            }
+            if (index[0]+1 < 8 && index[0]+1 >= 0 && index[1]-1 < 8 && index[1]-1 >= 0){
+              downLeft = [index[0]+1,index[1]-1];
+            }
+            if (index[0]+1 < 8 && index[0]+1 >= 0 && index[1]+1 < 8 && index[1]+1 >= 0){
+              downRight = [index[0]+1,index[1]+1];
+            }
+      
+      
+      
+      
+            let neighbors = [left, right, up, down, upLeft, upRight, downLeft, downRight];
+            // neighbors is a list of indexes that are neighbors to the mine and undefined if the neighbor is out of bounds
+            for (let j = 0; j < neighbors.length; j++){
+      
+              let indexArr = neighbors[j];
+              // if the neighbor is in bounds and is a safe cell update its inner html to be the number of mines around it
+              if (indexArr !== undefined){
+                if (gameCells[indexArr[0]][indexArr[1]].props.state === 1){
+                  let currCount = gameCells[indexArr[0]][indexArr[1]].props.count;
+                  currCount++;
+                  gameCells[indexArr[0]][indexArr[1]] = <SafeCell key={[indexArr[0],indexArr[1]]} i={indexArr[0]} j = {indexArr[1]} count = {currCount} state={1} />
+                }
+              } 
+            }
+          }
+            setGameBoard(gameCells);
+        }
+    
+    useEffect(() => {
+        generateBoard();
+    }, []);
+    
+    
     return(
         <>
         <div id='title'>
             <h1>Minesweeper</h1>
             <Menu />
         </div>
+        <div>
+            <button id='reset-button' onClick={generateBoard}>Reset</button>
+        </div>
+        
         <div id='game-container'>
-            <Gameboard />
+            <MinesweeperBoard gameCells={gameBoard}/>
         </div>
         
         </>
